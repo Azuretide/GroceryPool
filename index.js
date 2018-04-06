4f47691a57cc3aa14e5ae6e5d88a732b9df08d95
Azuretide <Azuretide@gmail.com>
2018-04-06 12:43:08 -0400

    dummy files

---
+++ index.js
@@ -0,0 +1,12 @@
+
+
+// Attaching events on document because then we can do it without waiting for
+// the DOM to be ready (i.e. before DOMContentLoaded fires)
+Util.events(document, {
+	// Final initalization entry point: the Javascript code inside this block
+	// runs at the end of start-up when the DOM is ready
+	"DOMContentLoaded": function() {
+		// Setup tasks
+
+	},
+});
