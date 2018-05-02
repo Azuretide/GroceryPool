f85027606c028a7431cf63b4d171e6d85d187009
Azuretide <Azuretide@gmail.com>
2018-05-01 21:45:15 -0400

    post gr4 cosmetics and safety

--- js/main.js
+++ js/main.js
@@ -1,9 +1,26 @@
+
+
 // Toggle user info block in menu
 function toggleInfo() {
     $('body').find('.user_info_header').toggleClass('active');
 };
 
-//Toggle disabled form on Edit page
-function toggleEdit(e) {
-    $(e.target).parent().parent().find('fieldset').removeAttr('disabled');
-}
+
+Util.events(document, {
+	"DOMContentLoaded": function() {
+		// console.log(localStorage.getItem('store'));
+		// localStorage.setItem('store', 'Wawa');
+
+		// Cancel Request Button
+		if (Util.one('#cancel-request')) {
+			Util.one('#cancel-request').addEventListener('click', (evt) => {
+				if (!confirm('Are you sure you want to delete this request?')) {
+        			evt.preventDefault();
+      			}
+			});
+		}
+		
+
+
+	},
+});
